4459d4f23a88e143962d449989f9aba672b51e8a
matypist <69389461+matypist@users.noreply.github.com>
2023-01-31 02:27:28 +0100

    Include Copyright Notice in code files

--- ssb/i18n/locales.py
+++ ssb/i18n/locales.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2022-2023, Matteo Collica (Matypist)
+#
+# This file is part of the "Telegram Groups Indexer Bot" (TGroupsIndexerBot)
+# project, the original source of which is the following GitHub repository:
+# <https://github.com/sapienzastudentsnetwork/tgroupsindexerbot>.
+#
+# TGroupsIndexerBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TGroupsIndexerBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with TGroupsIndexerBot. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 from json     import load   as json_load
 from os.path  import isfile as file_exists
